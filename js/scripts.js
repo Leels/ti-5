01f16b78cca16e636ddc77e65e149694a00801de
Student <example@example.com>
2019-10-23 14:19:46 -0700

    Add JS logic for calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,51 @@
-// //Addition
-//
-// var add = function(number1, number2) {
-// return number1 + number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter any number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(add(number1,number2));
-//
-// //subtraction
-//
-// var subtract= function(number1, number2) {
-// return number1 - number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter any number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(subtract(number1,number2));
-//
-// //multiplication
-// var multiply= function(number1, number2) {
-// return number1 * number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter any number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(multiply(number1,number2));
-//
-//
-// //division
-// var divide= function(number1, number2) {
-// return number1 / number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter any number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(divide(number1,number2));
+//Addition
 
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var subtract= function(number1, number2) {
+return number1 - number2;
+};
+
+var multiply= function(number1, number2) {
+return number1 * number2;
+};
+
+var divide= function(number1, number2) {
+return number1 / number2;
+};
+
+
+$(document).ready(function() {
+  $(".btn-info").click(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#number1").val());
+  var number2 = parseInt($("#number2").val());
+  $("#output").text(add(number1, number2));
+});
+
+$(".btn-success").click(function(event) {
+  event.preventDefault();
+var number1 = parseInt($("#number1").val());
+var number2 = parseInt($("#number2").val());
+$("#output").text(subtract(number1, number2));
+});
+
+$(".btn-danger").click(function(event) {
+  event.preventDefault();
+var number1 = parseInt($("#number1").val());
+var number2 = parseInt($("#number2").val());
+$("#output").text(multiply(number1, number2));
+});
+
+$(".btn-warning").click(function(event) {
+  event.preventDefault();
+var number1 = parseInt($("#number1").val());
+var number2 = parseInt($("#number2").val());
+$("#output").text(divide(number1, number2));
+});
+});
 //BMI
 
 // var bmi= function(weight, height) {
@@ -53,21 +58,23 @@
 //
 // alert(result.toFixed(2));
 
-var convertCelsius = function(fahrenheit) {
-return ((fahrenheit - 32) / 1.8);
-};
+//temperature converter
 
-var fahrenheit = parseFloat(prompt("Enter the temperature in degrees fahrenheit:"));
-
-var resultC = convertCelsius(fahrenheit);
-
-alert("Your temperature in celsius is " + resultC);
-
-var convertFahrenheit = function(celsius) {
-  return (celsius * 1.8 + 32);
-};
-var celsius = parseFloat(prompt("Enter the temperature in degrees celsius:"));
-
-var resultF = convertFahrenheit(celsius);
-
-alert("Your temperature in fahrenheight is " + resultF);
+// var convertCelsius = function(fahrenheit) {
+// return ((fahrenheit - 32) / 1.8);
+// };
+//
+// var fahrenheit = parseFloat(prompt("Enter the temperature in degrees fahrenheit:"));
+//
+// var resultC = convertCelsius(fahrenheit);
+//
+// alert("Your temperature in celsius is " + resultC);
+//
+// var convertFahrenheit = function(celsius) {
+//   return (celsius * 1.8 + 32);
+// };
+// var celsius = parseFloat(prompt("Enter the temperature in degrees celsius:"));
+//
+// var resultF = convertFahrenheit(celsius);
+//
+// alert("Your temperature in fahrenheight is " + resultF);
